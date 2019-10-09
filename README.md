884bd0a67d0a7d75c70a75235bfbc9a8dc28495d
Brad White <brad@voicebyte.com>
2019-10-09 11:11:20 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Tic Tac Toe - React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app) and started out as the React tutorial from their website. To imcrease my learning I will be applying any new techniques I come across to this project and pushing it beyond the starting point.
+
+## Current features
+This project started out as the tutotial from the React site. The current level of features is shown below. As the pipeline is developed (when I wish to have fun) it will be added to this section:
+1. Basic Tic Tac Toe board with move history - tutorial endpoint (https://reactjs.org/tutorial/tutorial.html)
+1. Extra tasks suggested in the wrap up section of tutorial - (https://reactjs.org/tutorial/tutorial.html#wrapping-up)
+ 
+
+## Pipeline
+
+These are the current items on my TODO list for added features, refactoring:
+
+1. Make the move list a separate component to Game and include it as others have been (i.e. proper component use)
+1. Allow the choice of who goes first X or O
+1. Provide a setup screen where players can enter their name and select X or O
+1. Add a randomiser to which starts first X or O
+1. Allow the addition of an Avatar for the user and use the avatar instead of X or O
+1. Allow a Play Computer option
+    1. first effort to be random selection (Easy mode)
+    1. second game tree (Hard mode)
+
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+To learn React, check out the [React documentation](https://reactjs.org/).
+
---
+++ src/index.js
@@ -0,0 +1,185 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square(props) {
+  let className = 'square '+props.winclass;
+  return (
+    <button 
+      className={className}
+      onClick={() => props.onClick()}
+    >
+    {props.value}
+    </button>
+  );
+}
+  
+class Board extends React.Component {
+  renderSquare(i, winclass) {
+    return (
+      <Square 
+        key={"square " + i}
+        winclass={winclass}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const boardItems = [];
+    const numRows = 3;
+    const numCols = 3;
+
+    console.log(this.props.winpattern);
+
+    // Outer loop to create row divs
+    for (let i = 0; i < numRows; i++) {
+      let columns = []
+      //Inner loop to create children
+      for (let j = 0; j < numCols; j++) {
+        let winclass = (this.props.winpattern.indexOf((i*numCols)+j) < 0)?'':'winsquare';
+        columns.push(this.renderSquare((i*numCols)+j, winclass))
+      }
+      //Create the parent and add the children
+      boardItems.push(<div key={"div"+i} className="board-row">{columns}</div>)
+    }
+
+    return (
+      <div>
+        {boardItems}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+        location: '[Row, Col]'
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+      moveorder: 'oldest',
+    };
+  }
+
+  handleClick(i){
+    const rowcollocation = [
+      '[1, 1]','[1, 2]','[1, 3]',
+      '[2, 1]','[2, 2]','[2, 3]',
+      '[3, 1]','[3, 2]','[3, 3]',
+    ];
+    
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+        location: rowcollocation[i],
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+  toggleMoveOrder(){
+    this.setState({
+      moveorder: (this.state.moveorder === 'newest') ? 'oldest' : 'newest',
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    const moveorder = this.state.moveorder;
+
+    const moves = history.map((step,move) => {
+      //step = currenct value, move = index
+      const desc = move ?
+        'Go to move #' + move + ' @ ' + history[move].location:
+        'Go to game start [Row, Col]';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>
+            {move === this.state.stepNumber ? <b>{desc}</b> : desc}
+          </button>
+        </li>
+      );
+    })
+
+    //reverse the order here!!
+    if(moveorder === 'newest'){
+      moves.reverse();
+    }
+
+    let status;
+    if(winner){
+      status = 'Winner: ' + winner.XorY;
+    }else{
+      status = (moves.length < 10)?'Next player: ' + (this.state.xIsNext ? 'X' : 'O'):'DRAW!';
+    }
+
+    return (
+      <div className="game">
+      <div className="game-board">
+        <Board 
+          squares={current.squares}
+          winpattern={winner ? winner.pattern : []}
+          onClick={(i) => this.handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <button onClick={() => this.toggleMoveOrder()}>
+            {this.state.moveorder === 'oldest' ? 'See Moves newset first' : 'See Moves oldest first'}
+        </button>
+        <ol>{moves}</ol>
+      </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+  for (let i = 0; i < lines.length; i++){
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return {XorY: squares[a],pattern:[a,b,c]};
+    }
+  }
+  return null;
+}
